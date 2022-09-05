5b2c9734e929df70c7099f1df81e28909c0d4fa3
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-09-05 19:17:19 +0200

    20220905

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import Analytics from './components/Analytics';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Newsletter from './components/Newsletter';
+import Card from './components/Card';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Analytics />
+      <Newsletter />
+      <Card />
     </div>
   );
 }
